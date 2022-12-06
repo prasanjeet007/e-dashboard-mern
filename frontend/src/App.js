05d6196d2f6a91ae62c22bdca0e6f520c5b1fd44
Prasanjeet Chatterjee <prasanjeetchatterjee28@gmail.com>
2022-12-06 12:18:59 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './Footer';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css';
+import {Link} from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <React.Fragment>
+        <ul className='navbar'>
+            <li><Link to="/">Products</Link></li>
+            <li><Link to="/add">Add Product</Link></li>
+            <li><Link to="/update">Update Product</Link></li>
+            <li><Link to="/logout">Logout</Link></li>
+            <li><Link to="/profile">Profile</Link></li>
+        </ul>
+    </React.Fragment>
+  )
+}
